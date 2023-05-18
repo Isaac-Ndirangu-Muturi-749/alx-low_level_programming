cb5fe8e96bae4a11d479f61934644d2debdb6d81
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-05-19 01:40:21 +0300

    update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,9 +7,9 @@
  */
 int main(void)
 {
-	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char *data = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, message, 59);
+	write(2, data, 59);
 
 	return (1);
 }
