73ca999242c9ebcb166ae96ec7731308a74e58ba
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-05-23 11:53:08 +0300

    update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,38 +1,32 @@
 #include <stdio.h>
 
 /**
- * main - Prints all possible different combinations of four digits
+ * main - Prints all possible different combinations of three digits
  *
  * Return: Always 0
  */
 int main(void)
 {
-	int num1, num2, num3, num4;
+	int num1, num2, num3;
 
-	for (num1 = 0; num1 <= 9; num1++)
+	for (num1 = 0; num1 <= 7; num1++)
 	{
-		for (num2 = num1 + 1; num2 <= 9; num2++)
+		for (num2 = num1 + 1; num2 <= 8; num2++)
 		{
 			for (num3 = num2 + 1; num3 <= 9; num3++)
 			{
-				for (num4 = num3 + 1; num4 <= 9; num4++)
+				putchar(num1 + '0');
+				putchar(num2 + '0');
+				putchar(num3 + '0');
+				if (num1 != 7 || num2 != 8 || num3 != 9)
 				{
-					putchar(num1 + '0');
-					putchar(num2 + '0');
-					putchar(num3 + '0');
-					putchar(num4 + '0');
-
-					if (num1 != 6 || num2 != 7 || num3 != 8 || num4 != 9)
-					{
-						putchar(',');
-						putchar(' ');
-					}
+					putchar(',');
+					putchar(' ');
 				}
 			}
 		}
 	}
 
 	putchar('\n');
-
 	return (0);
 }
