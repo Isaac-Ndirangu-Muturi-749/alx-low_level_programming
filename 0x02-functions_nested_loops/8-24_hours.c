44a1544638bc0a4e2df8fe44de28a3e17d33a09e
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-05-25 00:40:15 +0300

    update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,9 +7,11 @@
  */
 void jack_bauer(void)
 {
-	for (int hour = 0; hour < 24; hour++)
+	int hour, minute;
+
+	for (hour = 0; hour < 24; hour++)
 	{
-		for (int minute = 0; minute < 60; minute++)
+		for (minute = 0; minute < 60; minute++)
 		{
 			_putchar((hour / 10) + '0');
 			_putchar((hour % 10) + '0');
