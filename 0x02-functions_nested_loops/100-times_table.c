90920c09c261294843efd870aeceab013ec3d790
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-05-25 02:16:55 +0300

    update 100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the n times table, starting with 0
+ * @n: n times table
+ */
+void print_times_table(int n)
+{
+	int row, col, prod;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (row = 0; i <= n; row++)
+		{
+			for (col = 0; col <= n; col++)
+			{
+				prod = row * col;
+				if (col == 0)
+				{
+					_putchar(prod + '0');
+				} else if (prod < 10 && col != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(prod + '0');
+				} else if (col >= 10 && prod < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((prod / 10) + '0');
+					_putchar((prod % 10) + '0');
+				} else if (prod >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10) % 10) + '0');
+					_putchar((prod % 10) + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
