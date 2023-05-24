93f424070af8783bb0fa409037235b180578a466
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-05-25 00:17:26 +0300

    update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,12 @@
  */
 int print_last_digit(int n)
 {
+	int last_digit = n % 10;
+
 	if (n < 0)
 	{
 		n = -n;
 	}
-
-	int last_digit = n % 10;
 
 	_putchar(last_digit + '0');
 	return (last_digit);
