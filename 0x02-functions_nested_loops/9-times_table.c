f99cfef4eb856e6f000857985b4e740a0f3fd493
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-05-25 01:28:16 +0300

    update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,36 +7,34 @@
  */
 void times_table(void)
 {
-    int row, column, product;
+	int row, column, product;
 
-    for (row = 0; row <= 9; row++)
-    {
-        for (column = 0; column <= 9; column++)
-        {
-            product = row * column;
-            if (column == 0)
-            {
-                _putchar('0');
-            }
-            else if (product < 10)
-            {
-                _putchar(' ');
-                _putchar(' ');
-                _putchar(product + '0');
-            }
-            else
-            {
-                _putchar(' ');
-                _putchar((product / 10) + '0');
-                _putchar((product % 10) + '0');
-            }
-
-            if (column != 9)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-        }
-        _putchar('\n');
-    }
+	for (row = 0; row <= 9; row++)
+	{
+		for (column = 0; column <= 9; column++)
+		{
+			product = row * column;
+			if (column == 0)
+			{
+				_putchar('0');
+			}
+			else if (product < 10)
+			{
+				_putchar(' ');
+				_putchar(product + '0');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
+			}
+			if (column != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
 }
