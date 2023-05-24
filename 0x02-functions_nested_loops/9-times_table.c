692f21b3489249475a2f8b47e565a047add48d72
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-05-25 00:51:50 +0300

    update 9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0.
+ *
+ * Return: No return
+ */
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (int j = 1; j <= 9; j++)
+		{
+			_putchar(' ');
+			_putchar(i + '0');
+			_putchar(' ');
+			_putchar('*');
+			_putchar(j + '0');
+			_putchar(' ');
+			_putchar('=');
+			_putchar(' ');
+			_putchar(i * j + '0');
+			_putchar('\n');
+		}
+	}
+}
