dbda58bc186715c82284ec6351ee8b12239c1bda
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-05-26 02:41:41 +0300

    update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,22 +9,23 @@
  */
 void print_diagonal(int n)
 {
-	int i, j;
-
 	if (n <= 0)
 	{
 		_putchar('\n');
-		return;
-	}
-
-	for (i = 0; i < n; i++)
+	} else
 	{
-		for (j = 0; j < i; j++)
+		int i, j;
+
+		for (i = 0; i < n; i++)
 		{
-			_putchar(' ');
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
+			}
+			_putchar('\n');
 		}
-		
-		_putchar('\\');
-		_putchar('\n');
 	}
 }
