b881b488080172435755e14645f3fbc8c0496ab5
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-05-25 21:46:15 +0300

    update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,11 +8,11 @@
  */
 void print_numbers(void)
 {
-	int i;
+	char c;
 
-	for (i = '0'; i <= '9'; i++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		_putchar(i + '0');
+		_putchar(c);
 	}
 
 	_putchar('\n');
