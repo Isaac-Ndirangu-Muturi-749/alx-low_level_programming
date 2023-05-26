f98523741ca4090adff2e26bdf5589d7a7a037a8
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-05-26 04:00:55 +0300

    update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,20 +7,27 @@
  */
 void print_triangle(int size)
 {
-	int row, col;
-
 	if (size <= 0)
 	{
 		_putchar('\n');
-		return;
 	}
-
-	for (row = 0; row < size; row++)
+	else
 	{
-		for (col = 0; col <= i; col++)
+		int i, j;
+
+		for (i = 1; i <= size; i++)
 		{
-			_putchar('#');
+			for (j = i; j < size; j++)
+			{
+				_putchar(' ');
+			}
+
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
