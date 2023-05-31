9397ee799ad5cd01b7e9f9a18e9e46cd4731e0af
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-05-31 04:05:00 +0300

    update 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - a function that prints a string in reverse followed by a new line
+ * @s: pointer to the string
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+
+	for (int i = length - 1; i >= 0; i--)
+	{
+		putchar(s[i]);
+	}
+
+	putchar('\n');
+}
