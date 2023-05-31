6263cd741a69c270fe026bf365776287d9f0579a
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-05-31 04:16:00 +0300

    update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,7 +2,7 @@
 #include <string.h>
 
 /**
- * print_rev - a function that prints a string in reverse 
+ * print_rev - a function that prints a string in reverse
  * followed by a new line
  * @s: pointer to the string
  * Return: void
