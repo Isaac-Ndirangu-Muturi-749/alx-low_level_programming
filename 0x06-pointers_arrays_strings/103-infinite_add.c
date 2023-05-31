75799b4201d5be493b250ee0c1a335ffb48d3608
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-05-31 23:02:14 +0300

    update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -39,7 +39,6 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int overflow = 0, i = 0, j = 0, digits = 0;
 	int val1 = 0, val2 = 0, temp_tot = 0;
-
 	/* Find the lengths of the input strings */
 	while (*(n1 + i) != '\0')
 		i++;
@@ -47,16 +46,13 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		j--;
 	i--;
 	j--;
-
 	/* Check if input lengths exceed buffer size */
 	if (j >= size_r || i >= size_r)
 		return (0);
-
 	/* Perform addition digit by digit */
 	while (j >= 0 || i >= 0 || overflow == 1)
 	{
-		/* Get the current digits to be added */
-		if (i < 0)
+		if (i < 0)/* Get the current digits to be added */
 			val1 = 0;
 		else
 			val1 = *(n1 + i) - '0';
@@ -64,30 +60,20 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			val2 = 0;
 		else
 			val2 = *(n2 + j) - '0';
-
 		temp_tot = val1 + val2 + overflow; /* Add digits and overflow */
-
-		/* Check if there is an overflow */
-		if (temp_tot >= 10)
+		if (temp_tot >= 10)/* Check if there is an overflow */
 			overflow = 1;
 		else
 			overflow = 0;
-
-		/* Check if result exceeds buffer size */
-		if (digits >= (size_r - 1))
+		if (digits >= (size_r - 1))/*Check if result exceed buffersize*/
 			return (0);
-
-		/* Store the digit in the result buffer */
 		*(r + digits) = (temp_tot % 10) + '0';
-		digits++;
+		digits++;/* Store the digit in the result buffer */
 		j--;
 		i--;
 	}
-
-	/* Check if the result fills the buffer completely */
-	if (digits == size_r)
+	if (digits == size_r)/* Check if the result fills buffer completely */
 		return (0);
-
 	*(r + digits) = '\0'; /* Add null terminator to the result string */
 	rev_string(r); /* Reverse the result string */
 	return (r); /* Return the result string */
