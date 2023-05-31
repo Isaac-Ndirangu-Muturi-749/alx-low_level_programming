9b771c2b039d3a194b12c3f27c6db886748fbfde
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-05-31 22:00:54 +0300

    update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,37 +1,42 @@
-#include "main.h"
+#include <stdio.h>
+#include <math.h>
 
 /**
- * largest_prime_factor - Finds the largest prime factor of a given number
- * @n: The number
- *
- * Return: The largest prime factor
+ * main - finds and prints the largest prime factor of the number 612852475143
+ * followed by a new line
+ * Return: Always 0 (Success)
  */
-long largest_prime_factor(long n)
+int main(void)
 {
-	long i, largest_factor;
+	long int n;       /* Variable to store the number*/
+	long int max;     /*Variable to store the largest prime factor*/
+	long int i;       /* Loop variable*/
 
-	largest_factor = -1;
+	n = 612852475143;
+	max = -1;         /* Initialize max to -1*/
 
-	/*Divide n by 2 until it is no longer divisible by 2*/
+	/* Divide n by 2 until it is no longer divisible by 2*/
 	while (n % 2 == 0)
 	{
-		largest_factor = 2;
-		n /= 2;
+		max = 2;       /* Update max to 2*/
+		n /= 2;        /* Divide n by 2*/
 	}
 
-	/*Check for prime factors starting from 3*/
-	for (i = 3; i * i <= n; i += 2)
+	 /* Check for prime factors starting from 3*/
+	for (i = 3; i <= sqrt(n); i = i + 2)
 	{
+		/* Divide n by i until it is no longer divisible by i*/
 		while (n % i == 0)
 		{
-			largest_factor = i;
-			n /= i;
+			max = i;    /* Update max to i*/
+			n = n / i;  /* Divide n by i*/
 		}
 	}
 
-	/*If n is a prime number greater than 2, it becomes the largest factor*/
-	if (n > 2)
-		largest_factor = n;
+	if (n > 2)   /* If n is greater than 2, it is a prime factor itself*/
+		max = n;  /* Update max to n*/
 
-	return (largest_factor);
+	printf("%ld\n", max);   /*/ Print the largest prime factor*/
+
+	return (0);
 }
