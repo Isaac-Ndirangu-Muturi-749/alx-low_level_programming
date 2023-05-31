9b7cec8630dd5d45e433bc8443b85941fe008c68
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-05-31 20:16:15 +0300

    update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,22 +1,26 @@
 #include "main.h"
 
 /**
- * print_number - Prints an integer.
- * @n: The integer to be printed.
+ * print_number - print number
+ * @n: integer
+ * Return: 0
  */
+
 void print_number(int n)
 {
-	unsigned int n1;  /*Variable to store the absolute value of n*/
+	unsigned int n1;
 
-	n1 = (n < 0) ? -n : n;  /* Assign absolute value of n to n1*/
+	n1 = n;
 
 	if (n < 0)
-		_putchar('-');  /*Print '-' sign for negative numbers*/
+	{
+		_putchar('-');
+		n1 = -n;
+	}
 
-	/*Check if there are more digits to print*/
-	/*Recursively call the function to print remaining digits*/
 	if (n1 / 10 != 0)
+	{
 		print_number(n1 / 10);
-
-	_putchar((n1 % 10) + '0');  /*Print the rightmost digit using _putchar*/
+	}
+	_putchar((n1 % 10) + '0');
 }
