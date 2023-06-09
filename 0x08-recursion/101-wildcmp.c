32ece6e19724c6a80a3199c628301fcc524c6649
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-06-10 02:27:30 +0300

    update 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -8,21 +7,34 @@
  *
  * Return: 1 if the strings are identical, 0 otherwise
  */
+
 int wildcmp(char *s1, char *s2)
 {
-	/* Base cases: if both strings are empty, they are identical */
-	if (*s1 == '\0' && *s2 == '\0')
-		return (1);
+	/* Base case: If s1 is empty */
+	if (*s1 == '\0')
+	{
+		/* If s2 is not empty and s2 has a wildcard '*',*/
+		/* move to the next character in s2 */
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		/* If s2 also empty, the strings can be considered identical */
+		return (*s2 == '\0');
+	}
 
-	/* If the current characters match */
-	/* or s2 has a wildcard '*', move to the next characters */
-	if (*s1 == *s2 || *s2 == '*')
-		return (wildcmp(s1 + 1, s2 + 1));
-
-	/* If s2 has a wildcard '*', */
-	/* try matching s1 with and without the current character in s2 */
+	/* If s2 has a wildcard '*', consider two cases: */
+	/* match s1 with and without the current character in s2 */
 	if (*s2 == '*')
-		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	/* If the current characters of s1 and s2 match, */
+	/*move to the next characters */
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
 
 	/* If none of above conditions are met, the strings are not identical */
 	return (0);
