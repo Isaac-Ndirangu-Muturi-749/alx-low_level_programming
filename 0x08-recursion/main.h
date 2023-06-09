ffa6df66579d876c906da7be2a8ac93e505c4cc5
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-06-10 00:17:40 +0300

    update 6-is_prime_number.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,6 +10,7 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int sqrt_helper(int n, int start, int end);
 int is_prime_number(int n);
+int is_prime_helper(int n, int divisor);
 int is_palindrome(char *s);
 
 #endif
