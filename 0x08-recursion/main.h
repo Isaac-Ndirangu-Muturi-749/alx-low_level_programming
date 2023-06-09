a272808dcc3a6e4b5e96de8b485bb693ef4ff073
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-06-09 22:53:23 +0300

    update 5-sqrt_recursion.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,6 +8,7 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int sqrt_helper(int n, int start, int end);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 
