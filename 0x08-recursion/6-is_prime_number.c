f4a6db00e6f65bad0c268b2a77b18f695e1607b2
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-06-10 00:07:16 +0300

    update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,18 +8,16 @@
  */
 int is_prime_number(int n)
 {
-	int i;
-
 	/* Base cases */
 	if (n <= 1)
-		return (0);  /* Numbers less than or equal to 1 are not prime*/
+		return (0); /* Numbers less than or equal to 1 are not prime */
 
 	/* Check for divisibility */
-	for (i = 2; i * i <= n; i++)
+	for (int i = 2; i * i <= n; i++)
 	{
 		if (n % i == 0)
-			return (0);  /* n is divisible by i, so it is not prime*/
+			return (0); /* n is divisibleby i, so it is not prime */
 	}
 
-	return (1);  /* n is not divisible by any numbers, so it is prime*/
+	return (1); /* n is not divisible by any numbers, so it is prime */
 }
