5f7af4ee51326aa54e7a2e4b67d6a551682ccf46
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-06-10 02:10:55 +0300

    update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,6 +2,31 @@
 #include <string.h>
 
 /**
+ * is_palindrome_recursive - recursive helper function to
+ * check if a string is a palindrome
+ * @s: input string
+ * @start: starting index
+ * @end: ending index
+ *
+ * Return: 1 if the string is a palindrome, 0 otherwise
+ */
+int is_palindrome_recursive(char *s, int start, int end)
+{
+	/* Base case: */
+	/* an empty string or a string with asingle character is a palindrome */
+	if (start >= end)
+		return (1);
+
+	/* Check if the characters at the start and end indices match */
+	if (s[start] != s[end])
+		return (0);
+
+	/* Recursively check the remaining substring */
+	/* without the start and end characters */
+	return (is_palindrome_recursive(s, start + 1, end - 1));
+}
+
+/**
  * is_palindrome - checks if a string is a palindrome
  * @s: input string
  *
@@ -10,21 +35,13 @@
 int is_palindrome(char *s)
 {
 	int length = strlen(s);
-	int i, j;
 
 	/* Base case: */
 	/*an empty string or a string with a single character is a palindrome */
 	if (length == 0 || length == 1)
 		return (1);
 
-	/* Compare characters from the beginning and the end of the string */
-	for (i = 0, j = length - 1; i < j; i++, j--)
-	{
-		/* Characters at the corresponding positions don't match,*/
-		/* not a palindrome */
-		if (s[i] != s[j])
-			return (0);
-	}
-
-	return (1); /* All characters match, the string is a palindrome */
+	/* Call the recursive helper function */
+	/*to check if the string is a palindrome */
+	return (is_palindrome_recursive(s, 0, length - 1));
 }
