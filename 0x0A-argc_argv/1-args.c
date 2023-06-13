d9b5e62fbb6fd051be3cbf84a4ac7a88c5d47e17
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-06-13 11:47:05 +0300

    update 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed into the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	/* Print the number of arguments (excluding the program name) */
+	printf("%d\n", argc - 1);
+
+	/* Return 0 to indicate successful execution */
+	return (0);
+}
