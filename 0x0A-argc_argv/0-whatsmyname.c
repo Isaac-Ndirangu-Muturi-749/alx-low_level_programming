e5ca47d6c7f58f5f6212872f645d1a7f3cf8bfb6
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-06-13 11:43:39 +0300

    g update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
- * main -  a program that prints its name, followed by a new line
+ * main - prints the name of the program
  * @argc: number of arguments
  * @argv: array of arguments
+ * __attribute__((unused)) - attribute is used to indicate to the
+ * compiler that the argc parameter is intentionally unused.
  *
  * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	/* Accessing the program name from argv[0]*/
-	printf("%s\n", argv[0]);
+	/* Print the name of the program */
+	/* first element of the argv array*/
+	printf("%s\n", *argv);
 
+	/* Return 0 to indicate successful execution */
 	return (0);
 }
