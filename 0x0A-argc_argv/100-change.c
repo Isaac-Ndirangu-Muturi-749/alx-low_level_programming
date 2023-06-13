2e087d97d7d77fde221fbebade4047ed69350b78
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-06-13 12:28:36 +0300

    update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -23,30 +23,18 @@ int main(int argc, char *argv[])
 		return (0);
 	}
 	/* Calculate the minimum number of coins */
-	while (cents >= 25)
+	for (; cents > 0; coins++)
 	{
-		cents -= 25;
-		coins++;
-	}
-	while (cents >= 10)
-	{
-		cents -= 10;
-		coins++;
-	}
-	while (cents >= 5)
-	{
-		cents -= 5;
-		coins++;
-	}
-	while (cents >= 2)
-	{
-		cents -= 2;
-		coins++;
-	}
-	while (cents >= 1)
-	{
-		cents -= 1;
-		coins++;
+		if (cents >= 25)
+			cents -= 25;
+		else if (cents >= 10)
+			cents -= 10;
+		else if (cents >= 5)
+			cents -= 5;
+		else if (cents >= 2)
+			cents -= 2;
+		else
+			cents -= 1;
 	}
 	printf("%d\n", coins);/* Print the result */
 	return (0);
