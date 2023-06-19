51c9374a8006129ba3202d1aa02bda789b508bc9
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-06-19 22:04:22 +0300

    update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - a program that prints the name of the file it was compiled from,
- * followed by a new line
+ * main - a program that prints the name of the file it was compiled from
  *
- * Return: 0 if successful
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	printf("%s\n", __FILE__);
