0fc0ca717f3b226dfc3f9c648277a9afff194f6f
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-06-20 10:30:12 +0300

    update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,15 +13,12 @@
  *
  * Return: void
  */
-void init_dog(struct dog* d, char* name, float age, char* owner)
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL) /*Check if the struct pointer is NULL*/
 	{
 		/* If so, allocate memory for the struct*/
 		d = malloc(sizeof(struct dog));
-		/* If memory allocation fails, return immediately*/
-		if (d == NULL)
-			return;
 	}
 
 	d->name = name; /* Assign the name to the struct's name member*/
