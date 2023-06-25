67dd0b0b1a7d862179e5820da93f0af895dee7d3
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-06-25 14:12:23 +0300

    update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -10,14 +10,13 @@
  *              as arguments. It checks for null pointers and then calls the
  *              function pointed to by `f` with the `name` as the argument.
  *
- * Return: void
+ * Return: nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
 	/* Check if either name or f is a null pointer*/
 	if (name == NULL || f == NULL)
 		return;
-
 	/* Call the function pointed to by f, passing the name as the argument*/
 	f(name);
 }
