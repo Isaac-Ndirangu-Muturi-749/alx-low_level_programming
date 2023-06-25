5433bbc45e328e06addccfc023f3d99c63f7abf6
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-06-25 14:13:29 +0300

    update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,22 +1,15 @@
 #include "function_pointers.h"
 #include <stdio.h>
-
 /**
  * print_name - print name using pointer to function
- * @name: string to print
- * @f: pointer to function that prints the name
- *
- * Description: This function takes a string `name` and a function pointer `f`
- *              as arguments. It checks for null pointers and then calls the
- *              function pointed to by `f` with the `name` as the argument.
- *
+ * @name: string to add
+ * @f: pointer to function
  * Return: nothing
- */
+ **/
 void print_name(char *name, void (*f)(char *))
 {
-	/* Check if either name or f is a null pointer*/
 	if (name == NULL || f == NULL)
 		return;
-	/* Call the function pointed to by f, passing the name as the argument*/
+
 	f(name);
 }
