8fcb8f8c6353bf208b43a010a1dedff3bed767ce
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-07-03 21:41:43 +0300

    update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-        size_t count = 0;
+	size_t count = 0;
 
-        while (h)/* Iterate through the linked list until NULL is reached*/
-        {
-                printf("%d\n", h->n);/* Print the value of the current node */
-                count++;/* Increment the count of nodes */
-                h = h->next;/* Move to the next node */
-        }
-
-        return (count);/* Return the number of nodes */
+	while (h)/* Iterate through the linked list until NULL is reached*/
+	{
+		printf("%d\n", h->n);/* Print the value of the current node */
+		count++;/* Increment the count of nodes */
+		h = h->next;/* Move to the next node */
+	}
+	return (count);/* Return the number of nodes */
 }
