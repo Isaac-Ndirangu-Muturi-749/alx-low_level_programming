fc41756f85a1b0d934de975d98ddd91585a8aee2
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-07-03 23:21:50 +0300

    update 102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -8,40 +8,38 @@
  */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t size = 0;
-	listint_t *current, *temp;
+	size_t len = 0;
+	int diff;
+	listint_t *temp;
 
 	/* Check if the list or head pointer is NULL */
 	if (h == NULL || *h == NULL)
-		return (size);
-
-	/* Start with the head of the list */
-	current = *h;
+		return (0);
 
 	/* Traverse the list */
-	while (current != NULL)
+	while (*h)
 	{
-		size++; /* Increment the size of the list */
+		diff = *h - (*h)->next;
 
 		/* Check if a loop is encountered */
-		if (current->next >= current)
+		if (diff > 0)
 		{
-			/* Break the loop */
-			temp = current;
-			current = current->next;
-			temp->next = NULL;
-			free(temp);
-			*h = NULL; /* Set the head pointer to NULL */
-			break;
+			temp = (*h)->next;
+			free(*h);
+			*h = temp;
+			len++;
 		}
 		else
 		{
-			/* Move to the next node and free the current node */
-			temp = current;
-			current = current->next;
-			free(temp);
+			/* Free the current node and break the loop */
+			free(*h);
+			*h = NULL;
+			len++;
+			break;
 		}
 	}
 
-	return (size);
+	*h = NULL; /* Set the head pointer to NULL */
+
+	return (len);
 }
