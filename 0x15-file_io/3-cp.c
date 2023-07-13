17e6bbe936e2af5d03a36f55e909cb2b0fd59f60
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-07-13 14:02:53 +0300

    update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -18,14 +18,12 @@ char *create_buffer(char *filename)
 	/* Check if memory allocation failed */
 	if (buffer == NULL)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
 		exit(99);
 	}
 
 	return (buffer);
 }
-
-
 
 /**
  * close_file - Closes file descriptors.
@@ -46,7 +44,6 @@ void close_file(int file_descriptor)
 	}
 }
 
-
 /**
  * main - Copies the contents of a file to another file.
  * @argc: The number of arguments supplied to the program.
@@ -55,13 +52,13 @@ void close_file(int file_descriptor)
  * Return: 0 on success.
  *
  * Description: If the argument count is incorrect - exit code 97.
- *             If file_from does not exist or cannot be read - exit code 98.
- *             If file_to cannot be created or written to - exit code 99.
- *             If file_to or file_from cannot be closed - exit code 100.
+ *              If file_from does not exist or cannot be read - exit code 98.
+ *              If file_to cannot be created or written to - exit code 99.
+ *              If file_to or file_from cannot be closed - exit code 100.
  */
 int main(int argc, char *argv[])
 {
-	int source_file, destination_file,  bytesread, byteswritten;
+	int source_file, destination_file, bytes_read, bytes_written;
 	char *buffer;
 
 	/* Check if the argument count is correct */
@@ -76,41 +73,58 @@ int main(int argc, char *argv[])
 
 	/* Open the source file for reading */
 	source_file = open(argv[1], O_RDONLY);
+	if (source_file == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		free(buffer);
+		exit(98);
+	}
 
 	/* Read from the source file into the buffer */
-	bytesread = read(source_file, buffer, 1024);
+	bytes_read = read(source_file, buffer, 1024);
+	if (bytes_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		free(buffer);
+		close_file(source_file);
+		exit(98);
+	}
 
 	/* Open the destination file for writing */
 	destination_file = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (destination_file == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		free(buffer);
+		close_file(source_file);
+		exit(99);
+	}
 
 	/* Loop until all data is read and written */
-	do {
-		/* Check if reading from the source file failed */
-		if (source_file == -1 || bytesread == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			free(buffer);
-			exit(98);
-		}
-
+	while (bytes_read > 0)
+	{
 		/* Write the buffer contents to the destination file */
-		w = write(to, buffer, r);
-
-		/* Check if writing to the destination file failed */
-		if (destination_file == -1 || byteswritten == -1)
+		bytes_written = write(destination_file, buffer, bytes_read);
+		if (bytes_written == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			free(buffer);
+			close_file(source_file);
+			close_file(destination_file);
 			exit(99);
 		}
 
 		/* Read from the source file into the buffer */
-		byteswritten = read(source_file, buffer, 1024);
-
-		/* Open the destination file in append mode */
-		destination_file = open(argv[2], O_WRONLY | O_APPEND);
-
-	} while (bytesread > 0);
+		bytes_read = read(source_file, buffer, 1024);
+		if (bytes_read == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			free(buffer);
+			close_file(source_file);
+			close_file(destination_file);
+			exit(98);
+		}
+	}
 
 	/* Free the buffer memory */
 	free(buffer);
