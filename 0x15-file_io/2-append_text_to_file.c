b43bd45989c32487f406a362640aff6e4bcb89bf
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-07-13 13:33:51 +0300

    update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -2,6 +2,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
 #include "main.h"
 /**
  * append_text_to_file - Write a function that appends text at the end of file
@@ -31,11 +32,12 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	/* Write the content to the file */
 	bytesWritten = write(fileDescriptor, text_content, strlen(text_content));
-	close(fileDescriptor);
+
 
 	/* Check if the write operation was successful */
 	if (bytesWritten == -1)
 		return (-1);
 
+	close(fileDescriptor);
 	return (1);
 }
