289fbd7c23302c6c26178f1ab692899b35e814e7
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-07-13 15:47:28 +0300

    update main.h

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -19,7 +19,8 @@ unsigned int binary_to_uint(const char *b)
 		/* Check if the character is not '0' or '1' */
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
-
+		/*multiplying the current value by 2 (left shift by 1) and*/
+		/*adding the decimal value of the binary digit to it.*/
 		/* Left shift the result by 1 and add the binary digit to it */
 		result = (result << 1) + (b[i] - '0');
 	}
