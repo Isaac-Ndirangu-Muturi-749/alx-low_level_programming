26b3a68794978d02bcde4683290cd391315bed4a
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-07-13 12:41:23 +0300

    update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,13 +9,16 @@
  *
  * @filename: text file to read
  * @letters: the number of letters it should read and print
+ *           if the file can not be opened or read, return 0
+ *           if filename is NULL return 0
+ *           if write fails or doesnt write expected amount of bytes, return 0
  *
  * Return: returns the actual number of letters it could read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t bytesRead, bytesWritten;
-	char buffer[letters];
+	char *buffer[letters];
 	int fileDescriptor;
 
 	/* Check if filename is NULL */
