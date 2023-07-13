a4dca0d84e22ea3cd71bbc2e7296d2c9dca799a3
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-07-13 13:55:54 +0300

    update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,73 +1,123 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
 
-#define BUFFER_SIZE 1024
-
-void print_error(const char *message)
+/**
+ * create_buffer - Allocates 1024 bytes for a buffer
+ * @filename: The name of the file
+ *
+ * Return: A pointer to the newly-allocated buffer.
+ */
+char *create_buffer(char *filename)
 {
-	dprintf(STDERR_FILENO, "Error: %s\n", message);
-}
+	char *buffer;
 
-int main(int argc, char *argv[])
-{
-	int fileFrom, fileTo;
-	ssize_t bytesRead, bytesWritten;
-	char buffer[BUFFER_SIZE];
+	/* Allocate memory for the buffer */
+	buffer = malloc(sizeof(char) * 1024);
 
-	/* Check if the number of arguments is correct */
-	if (argc != 3)
+	/* Check if memory allocation failed */
+	if (buffer == NULL)
 	{
-		print_error("Usage: cp file_from file_to");
-		exit(97);
-	}
-
-	/* Open the source file in read-only mode */
-	fileFrom = open(argv[1], O_RDONLY);
-	if (fileFrom == -1)
-	{
-		print_error(argv[1]);
-		exit(98);
-	}
-
-	/* Open the destination file in write-only mode, create it if it doesn't exist, and set permissions */
-	fileTo = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-	if (fileTo == -1)
-	{
-		print_error(argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
 		exit(99);
 	}
 
-	/* Read data from the source file and write it to the destination file */
-	while ((bytesRead = read(fileFrom, buffer, BUFFER_SIZE)) > 0)
+	return (buffer);
+}
+
+
+
+/**
+ * close_file - Closes file descriptors.
+ * @file_descriptor: The file descriptor to be closed.
+ */
+void close_file(int file_descriptor)
+{
+	int c;
+
+	/* Close the file descriptor */
+	c = close(file_descriptor);
+
+	/* Check if closing the file descriptor failed */
+	if (c == -1)
 	{
-		bytesWritten = write(fileTo, buffer, bytesRead);
-		if (bytesWritten == -1) {
-			print_error(argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_descriptor);
+		exit(100);
+	}
+}
+
+
+/**
+ * main - Copies the contents of a file to another file.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: 0 on success.
+ *
+ * Description: If the argument count is incorrect - exit code 97.
+ *             If file_from does not exist or cannot be read - exit code 98.
+ *             If file_to cannot be created or written to - exit code 99.
+ *             If file_to or file_from cannot be closed - exit code 100.
+ */
+int main(int argc, char *argv[])
+{
+	int source_file, destination_file,  bytesread, byteswritten;
+	char *buffer;
+
+	/* Check if the argument count is correct */
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	/* Create the buffer */
+	buffer = create_buffer(argv[2]);
+
+	/* Open the source file for reading */
+	source_file = open(argv[1], O_RDONLY);
+
+	/* Read from the source file into the buffer */
+	bytesread = read(source_file, buffer, 1024);
+
+	/* Open the destination file for writing */
+	destination_file = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	/* Loop until all data is read and written */
+	do {
+		/* Check if reading from the source file failed */
+		if (source_file == -1 || bytesread == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			free(buffer);
+			exit(98);
+		}
+
+		/* Write the buffer contents to the destination file */
+		w = write(to, buffer, r);
+
+		/* Check if writing to the destination file failed */
+		if (destination_file == -1 || byteswritten == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			free(buffer);
 			exit(99);
 		}
-	}
 
-	/* Check for read errors */
-	if (bytesRead == -1)
-	{
-		print_error(argv[1]);
-		exit(98);
-	}
+		/* Read from the source file into the buffer */
+		byteswritten = read(source_file, buffer, 1024);
 
-	/* Close the source and destination file descriptors */
-	if (close(fileFrom) == -1)
-	{
-		print_error("Can't close fd");
-		exit(100);
-	}
+		/* Open the destination file in append mode */
+		destination_file = open(argv[2], O_WRONLY | O_APPEND);
 
-	if (close(fileTo) == -1)
-	{
-		print_error("Can't close fd");
-		exit(100);
-	}
+	} while (bytesread > 0);
+
+	/* Free the buffer memory */
+	free(buffer);
+
+	/* Close the file descriptors */
+	close_file(source_file);
+	close_file(destination_file);
 
 	return (0);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -17,4 +17,7 @@ int create_file(const char *filename, char *text_content);
 /* Append the given text content to an existing file */
 int append_text_to_file(const char *filename, char *text_content);
 
+char *create_buffer(char *file);
+void close_file(int fd);
+
 #endif
