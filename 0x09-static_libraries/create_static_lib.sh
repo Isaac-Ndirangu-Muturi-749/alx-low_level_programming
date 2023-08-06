00c875d4407d1c802689c37097c9196658f4bb30
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-08-06 13:23:55 +0300

    Update create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-# Compile each .c file into an object file
+# Compile each .c file into .o file
 gcc -c *.c
 
-# Create the static library
-ar rcs liball.a *.o
+# Create the static library liball.a from the .o files
+ar -rc liball.a *.o
+
+# Index the static library
+ranlib liball.a
+
+# Clean up intermediate .o files
+rm *.o
+
+echo "Static library liball.a created successfully!"
