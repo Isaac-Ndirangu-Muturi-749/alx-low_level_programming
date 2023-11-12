a3d57765c78444a1d924512e58b1335f1185aca4
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-11-12 15:39:51 +0300

    Update 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,14 +9,14 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t count = 0; /* Initialize count to 0 */
+	size_t count = 0; /* Initialize count to 0 */
 
-    while (h != NULL)
-    {
-        printf("%d\n", h->n); /* Print the data in the current node */
-        h = h->next; /* Move to the next node */
-        count++; /* Increment the count */
-    }
+	while (h != NULL)
+	{
+		printf("%d\n", h->n); /* Print the data in the current node */
+		h = h->next; /* Move to the next node */
+		count++; /* Increment the count */
+	}
 
-    return count; /* Return the number of nodes */
+	return (count); /* Return the number of nodes */
 }
