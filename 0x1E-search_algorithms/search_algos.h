eea1e965a181b5f6a1d6ec0af6515f535e454c5f
Isaac Ndirangu Muturi <ndirangumuturi749@gmail.com>
2024-03-13 19:40:59 +0300

    Update search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -10,5 +10,7 @@ int binary_search(int *array, size_t size, int value);
 void print_array(int *array, size_t start, size_t end);
 int advanced_binary_recursive(int *array, size_t left, size_t right, int value);
 int advanced_binary(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int binary_search_recursive(int *array, size_t start, size_t end, int value);
 
 #endif
