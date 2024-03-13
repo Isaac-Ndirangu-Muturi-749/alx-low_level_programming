6aa3650e3b87a3daa9b7bb8469d9ec4f825fa8b0
Isaac Ndirangu Muturi <ndirangumuturi749@gmail.com>
2024-03-13 19:19:55 +0300

    Update search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,10 +2,13 @@
 #define SEARCH_ALGOS_H
 
 #include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_array(int *array, size_t start, size_t end);
-
+int advanced_binary_recursive(int *array, size_t left, size_t right, int value);
+int advanced_binary(int *array, size_t size, int value);
 
 #endif
